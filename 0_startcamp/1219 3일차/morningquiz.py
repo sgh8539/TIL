9826eac37305b0263680b772442ddfa3da009074
songgunho <sgh8539@naver.com>
2018-12-19 09:55:59 +0900

    practicing git

---
+++ 0_startcamp/1219 3일차/morningquiz.py
@@ -0,0 +1,17 @@
+print('good morning')
+#1 평균을 구해라
+
+my_socre = [79, 84, 66, 93]
+
+
+i=[1,2,3,4]
+for i in my_socre:
+
+
+your_score ={
+    '수학' : 87,
+    '국어' : 83,
+    '영어' : 76,
+    '도덕' : 100
+}
+
