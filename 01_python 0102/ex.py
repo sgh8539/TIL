516a9cea833cab21ad40020d2b0957740da42669
songgunho <sgh8539@naver.com>
2019-01-07 17:43:40 +0900

    ss3|python|190107

--- 01_python 0102/ex.py
+++ 01_python 0102/ex.py
@@ -1,5 +1,27 @@
-global a
-def k() :
-    global a
-    a = 1
-    return a
+year_input = 2019
+yoon = year_input%100 != 0 and year_input%4 == 0 or year_input%400 ==0
+if yoon :
+    calander = [31,29,31,30,31,30,31,31,30,31,30,31]
+else:
+    calander = [31,28,31,30,31,30,31,31,30,31,30,31]
+year = year_input-1
+first_number = ((year*365+int(year/4)-int(year/100)+int(year/400))+1)%7
+
+print(year_input,'년')
+for j in range(0,12):
+    print(f'\n\n        {j+1}월')
+    print('일 월 화 수 목 금 토\n'+'---'*7)
+    print('   '*first_number,end='')
+    for i in range(1,calander[j]+1):
+        if (i+first_number)%7==0 and i<10:
+            print(i,end='  \n')
+        elif (i+first_number)%7==0 and i>9:
+            print(i,end=' \n')
+        elif i>9:
+            print(i,end=' ')
+        else:
+            print(i,end='  ')
+    first_number = (first_number+calander[j])%7
+
+
+
--- 01_python 0102/pi.py
+++ 01_python 0102/pi.py
@@ -1,4 +1,20 @@
-import ex
-a = ex.k()
-print(a)
+coupon = 11
+
+
+def chicken(n):
+    free = 3
+    if n<free:
+        return (0, n)
+    else:
+        return (1+chicken(n-free+1)[0], chicken(n-free+1)[1])
+
+a,b=chicken(coupon)
+print(f'치킨:{a}\n남은쿠폰:{b}')
+
+def chicchic(coup,free):
+    if coup<free:
+        return 0,free
+    else:
+        return(1+chicchic((coup-free+1),free)[0],free)
+print(chicchic(100,3))
 
